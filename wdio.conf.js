09a741d6d96d8fb0e89947f31344584458c367d4
Ashwin Gonsalves <ashwing@Ashwins-MacBook-Pro.local>
2018-11-14 21:44:59 +0530

    added changes for plugin

--- wdio.conf.js
+++ wdio.conf.js
@@ -8,8 +8,8 @@ exports.config = {
     // should work too though). These services define specific user and key (or access key)
     // values you need to put in here in order to connect to these services.
     //
-    user: process.env.BROWSERSTACK_USER,
-    key: process.env.BROWSERSTACK_KEY,
+    user: process.env.BROWSERSTACK_USERNAME,
+    key: process.env.BROWSERSTACK_ACCESS_KEY,
     
     
     //
