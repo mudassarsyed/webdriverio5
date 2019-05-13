dcbb7dea8cbd86c7ede231c0af71a8762c0ffc4c
Ashwin Gonsalves <ashwingonsalves@gmail.com>
2019-05-13 19:41:38 +0530

    Update spec0.js

--- test/specs/spec0.js
+++ test/specs/spec0.js
@@ -2,10 +2,8 @@ var assert = require('assert');
 
 describe('webdriver.io page', function() {
     it('should have the right title - the fancy generator way', function () {
-        // browser.url('http://www.whatsmyip.org/')
-        // browser.pause(5000);
         browser.url('http://webdriver.io');
         var title = browser.getTitle();
-        assert.equal(title, 'WebdriverIO - WebDriver bindings for Node.js');
+        assert.equal(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
     });
 });
